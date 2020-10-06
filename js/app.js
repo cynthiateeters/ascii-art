793f8130a7f21d9a9696b37d4fd6ef8ce690480e
cynthiateeters <glean@cynthiateeters.com>
2020-10-06 09:47:29 -0400

    add replaceAll()
    add comments & footer with link to resource

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-/* 
+/*   
 
-Teeters,Cynthia
-    
+asciiArt
+
+Take lines of ascii art and output it to a webpage
 
   _O/                   ,
      \                  /           \O_
@@ -24,10 +25,17 @@ const asciiArt = [
   '            /\\|/\\.                `',
 ];
 
+/* 
+  Get the elements from the webpage
+  using query of their classes
+*/
 const ascii = document.querySelector('.ascii');
-console.log(ascii);
 const asciip = document.querySelector('.ascii-p');
-console.log(asciip);
+
+/* 
+  Loop through the array of lines
+  and add a new pre elemet for each line
+*/
 
 for (let i = 0; i < asciiArt.length; i++) {
   let pre = document.createElement('pre');
@@ -35,9 +43,29 @@ for (let i = 0; i < asciiArt.length; i++) {
   ascii.appendChild(pre);
 }
 
+function replaceAll(string, search, replace) {
+  /* 
+   Chain together 2 methods
+
+   Split: a string method to split a string into an array of substrings
+
+   Join: an array method to create and return a new string by concatenating 
+   all of the elements in an array 
+  
+  */
+  return string.split(search).join(replace);
+}
+
+/* 
+  Loop through the array of lines
+  and add a new p elemet for each line
+*/
 for (let i = 0; i < asciiArt.length; i++) {
   let p = document.createElement('p');
-  let str = asciiArt[i].replace(/ /g, '\xa0');
+
+  // must replace regular spaces with non-breaking spaces
+  let str = replaceAll(asciiArt[i], ' ', '\xa0');
+
   p.innerText = str;
   asciip.appendChild(p);
 }
